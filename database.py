58658097c810be38dde5b00f09462c8ea397b365
will-huiyuan <118071171+will-huiyuan@users.noreply.github.com>
2023-09-15 18:45:31 -0400

    Update database.py
    basic

---
+++ database.py
@@ -0,0 +1,9 @@
+class Database
+  def __init__(account,others):
+    self.account = account
+    self.action ={"time":[],"action":[],"ip":[]}
+    # 记录每个新分钟的第一个元素的index
+    self.time_index = []
+
+  def sort_time(something):
+    pass
