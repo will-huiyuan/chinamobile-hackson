756215ec8fdd2de1ba79b40969efe982bd8927dc
Angel <2162556631@qq.com>
2023-09-16 17:10:22 -0400

    Preprocess.py completed draft 1

--- run.py
+++ run.py
@@ -8,17 +8,11 @@ import preprocess
 from datetime import datetime
 #-----
 
-def main(to_pred_path,result_save_path):
-    test1= database.Database("a2v3c4d5f")
-    test1.add_entry(datetime(2023, 9, 15, 10, 0), "Login", "192.168.0.1")
-    test1.add_entry(datetime(2023, 9, 15, 11, 30), "Logout", "192.168.0.2")
-    test1.add_entry(datetime(2023, 9, 16, 11, 30), "Logout", "192.168.0.2")
-    test1.add_entry(datetime(2023, 9, 15, 10, 0), "Login", "193.168.0.4")
-    test1.add_entry(datetime(2023, 9, 15, 11, 31), "Logout", "122.456.2.3")
-    test1.sort_time()
-    test1.print_entries()
+def main(file_path,result_save_path):
+    preprocess.main(file_path)
 
-main(0,0)
+
+main("test.csv",0)
 
 '''if __name__ == "__main__":
     to_pred_path  = sys.argv[1] # 数据路径
