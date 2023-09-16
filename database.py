4b37fada8c34eaad879a4c154dfa78a56c2b209b
Angel <2162556631@qq.com>
2023-09-16 02:56:52 -0400

    Database class draft 1

--- database.py
+++ database.py
@@ -1,9 +1,27 @@
-class Database
-  def __init__(account,others):
+class Database:
+  def __init__(self,account):
     self.account = account
-    self.action ={"time":[],"action":[],"ip":[]}
+    self.operations ={"time":[],"action":[],"ip":[]}
     # 记录每个新分钟的第一个元素的index
-    self.time_index = []
+    self.time_index = [0]
 
-  def sort_time(something):
-    pass
+  def sort_time(self):
+    self.operations=sorted(zip(self.operations['time'], self.operations['action'], self.operations['ip']))
+
+    curr=self.operations[0][0]
+    for o in range(len(self.operations)):
+      if self.operations[o][0]!=curr:
+        self.time_index.append(o)
+
+  def add_entry(self, time, action, ip):
+    self.operations["time"].append(time)
+    self.operations["action"].append(action)
+    self.operations["ip"].append(ip)
+
+#for testing----
+  def print_entries(self):
+    for entry in self.operations:
+      time, action, ip = entry
+      print(f"Time: {time}, Action: {action}, IP: {ip}")
+    print(f"time_index: {self.time_index}")
+#----
