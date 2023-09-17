30e67d694ade3ac14c77a8ef30ddeb6fffdadd6d
will-huiyuan <118071171+will-huiyuan@users.noreply.github.com>
2023-09-17 16:35:44 -0400

    Zxp (#8)
    * output重做，更新unusualcheck

--- output.py
+++ output.py
@@ -2,7 +2,7 @@ class Output:
     def __init__(self, path):
         self.path = path
         self.output=""
-
+    
     def append(self, content: list):
         self.output+=f"{content[0]},{content[1].strftime('%Y-%m-%d %H:%M')},{content[2]}\n"
 
---
+++ run.py
@@ -0,0 +1,26 @@
+import sys
+import database
+import os
+from algorithm import singly_linked_list
+import preprocess
+import unusual_check
+import output
+from datetime import datetime
+
+
+def main(file_path,result_save_path):
+    actions = preprocess.main(file_path)
+    out = output.Output(result_save_path)
+    for action in actions:
+        unusual_check.unusual_ip(action,output)
+        unusual_check.unusual_login(action,output)
+        unusual_check.not_in_worktime(action,output)
+    out.save()
+
+
+main("test.csv",0)
+
+'''if __name__ == "__main__":
+    to_pred_path  = sys.argv[1] # 数据路径
+    result_save_path = sys.argv[2] # 输出路径
+    main(to_pred_path,result_save_path) # 运行主函数'''
