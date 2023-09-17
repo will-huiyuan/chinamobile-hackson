d0a87b98ac567f0e78ea1c5ede729e6100fe9a17
Will Huiyuan <xuanpu.zhang@mail.mcgill.com>
2023-09-16 22:10:38 -0400

    run.py可用 not_in_worktime，unusual_login 完成

--- output.py
+++ output.py
@@ -3,10 +3,9 @@ class Output:
     def __init__(self, path):
         self.path = path
         self.file = open(path, 'w', encoding='utf-8')
-
-        self.output=""
+        self.board=""
 
 
     def append(self, content: list):
-        self.output+=f"{content[0]},{content[1].strftime('%Y-%m-%d %H:%M')},{content[2]}\n"
+        self.board+=f"{content[0]},{content[1].strftime('%Y-%m-%d %H:%M')},{content[2]}\n"
 
--- unusual_check.py
+++ unusual_check.py
@@ -1,7 +1,7 @@
 import database
 import output
 from datetime import datetime
-def unusual_login(database:database, output):
+def unusual_ip(database:database, output):
     if len(database.operations) >= 100:
         uncommon_ips = {}
         usual_ip = set()
@@ -10,35 +10,31 @@ def unusual_login(database:database, output):
             ip = log_entry[2]  # IP地址在元组中的第三个位置
             if (ip not in usual_ip):
                 if (ip in uncommon_ips):
-
                     if uncommon_ips[ip][0] < 5:
                         uncommon_ips[ip][0] += 1
                         uncommon_ips[ip][1].append(log_entry)
                     else:
-                        usual_ip.add(ip)
                         del uncommon_ips[ip]
+                        usual_ip.add(ip)
                 else:
-                    uncommon_ips[ip] = [1, database.operations]
+                    uncommon_ips[ip] = [1, [log_entry]]
 
-        for err_set in uncommon_ips:
-            output.append([database.account,err_set[1][0],"非常用IP访问"])
+        for key in uncommon_ips:
+            for log in uncommon_ips[key][1]:
+                output.append([database.account,log[0],"非常用IP访问"])
 
 
 def not_in_worktime(database:database,output:output):
-    # 每天正常工作时间为8:00~19:00，其余为非工作时间，请筛选出非工作时间的登录情况并输出到output中
-        # 定义工作时间
-        start_work_time = 8  # 8:00
-        end_work_time = 19  # 19:00
-
+        start_work_time = 8
+        end_work_time = 19
+        operations = database.operations
         # 遍历数据库中的所有登录条目
-        for log_entry in database.operations["time"]:
+        for t_index in database.time_index:
             # 解析时间
-            entry_time = datetime.strptime(log_entry, '%Y-%m-%d %H:%M')
-
-            # 检查是否在非工作时间
-            if entry_time.hour < start_work_time or entry_time.hour >= end_work_time:
-                output.append([database.account, entry_time, "非工作时间访问"])
+            now_time = operations[t_index][0].hour
+            if (now_time < start_work_time) or (now_time > end_work_time):
+                output.append([database.account, operations[t_index][0], "非工作时间访问"])
 
 
-def unusual_ip(database:database,output):
+def unusual_login(database:database,output):
     pass
