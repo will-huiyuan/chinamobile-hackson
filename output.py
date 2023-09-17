890324a3481a4df3ca5e33573d0e757634bc3cb3
Angela <113480613+Angelawork@users.noreply.github.com>
2023-09-17 16:19:45 -0400

    Draft 1 of output.py

--- output.py
+++ output.py
@@ -1,11 +1,15 @@
-
 class Output:
     def __init__(self, path):
         self.path = path
-        self.file = open(path, 'w', encoding='utf-8')
-        self.board=""
-
+        self.output=""
 
     def append(self, content: list):
-        self.board+=f"{content[0]},{content[1].strftime('%Y-%m-%d %H:%M')},{content[2]}\n"
+        self.output+=f"{content[0]},{content[1].strftime('%Y-%m-%d %H:%M')},{content[2]}\n"
+
+    def save(self):
+        self.file = open(self.path, 'w', encoding='utf-8')
+        self.file.write("异常账号,异常时间,异常类型\n")
+        self.file.write(f"{self.output}\n")
+        print(f"output file saved at {self.path}")
+        self.file.close()
 
