2040e2798360b59800144dda0728eb167b408453
will-huiyuan <118071171+will-huiyuan@users.noreply.github.com>
2023-09-18 19:16:56 -0400

    run 可用

--- run.py
+++ run.py
@@ -20,9 +20,7 @@ def main(file_path,result_save_path):
     out.save()
 
 
-main("trainA.csv","out_new.csv")
-
-'''if __name__ == "__main__":
+if __name__ == "__main__":
     to_pred_path  = sys.argv[1] # 数据路径
     result_save_path = sys.argv[2] # 输出路径
-    main(to_pred_path,result_save_path) # 运行主函数'''
+    main(to_pred_path,result_save_path) # 运行主函数
