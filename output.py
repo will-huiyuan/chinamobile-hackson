9aa8a20d59db0af592179638f85143da34526779
will-huiyuan <118071171+will-huiyuan@users.noreply.github.com>
2023-09-23 17:15:27 -0400

    Add files via upload

--- output.py
+++ output.py
@@ -9,7 +9,7 @@ class Output:
     def save(self):
         self.file = open(self.path, 'w', encoding='utf-8')
         self.file.write("异常账号,异常时间,异常类型\n")
-        self.file.write(f"{self.output}\n")
+        self.file.write(self.output)
         print(f"output file saved at {self.path}")
         self.file.close()
 
