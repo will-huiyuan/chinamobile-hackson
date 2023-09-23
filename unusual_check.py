567d82c0312d2838a6b92c622ce0b0fd977d4a99
Kiana Ren <125711690+KianaRen@users.noreply.github.com>
2023-09-23 17:20:06 -0400

    添加两个function的comment

--- unusual_check.py
+++ unusual_check.py
@@ -82,29 +82,35 @@ def unusual_login(database: database, output):
                     break
 
 
+# 检查高频业务访问
 def high_frequency_visit(database, output):
+    # 记录该账号不同分钟的业务以及对应的时间
     business_list = []
     time_list = []
 
     i = 0
+    # loop through 单个不同分钟的所有访问记录
     while i < len(database.time_index)-1:
+        # 记录单个分钟所有的访问记录
         business_sublist = []
-
+        # 记录当前时间，time_list和business_list中的index一一对应
         time_list.append(database.operations[database.time_index[i]][0])
         for j in range(database.time_index[i], database.time_index[i+1]):
             business_sublist.append(database.operations[j][1])
-
+        # 将该一分钟访问的业务sublist添加到总业务list
         business_list.append(business_sublist)
         i += 1
 
+    # 防止 index out of bound，单独处理最后一个不同分钟的访问记录
     business_sublist = []
     for j in range(database.time_index[i], len(database.operations)):
         business_sublist.append(database.operations[j][1])
     business_list.append(business_sublist)
     time_list.append(database.operations[database.time_index[i]][0])
 
+    # loop through 每分钟的业务访问记录
     for m in range(len(business_list)):
-
+        # 记录不同业务的名称以及对应的访问次数
         business_record = []
         count_record = []
         for business in business_list[m]:
@@ -113,37 +119,43 @@ def high_frequency_visit(database, output):
                 count_record.append(0)
             else:
                 count_record[business_record.index(business)] += 1
-
+        # loop through 所有业务的访问次数，每次达到五次及以上，则输出异常提示
         for num in count_record:
             if num >= 5:
                 output.append([database.account, time_list[m], "业务高频访问"])
-                # break 65
 
 
+# 检查账号复用
 def account_repeat(database, output):
+    # 记录该账号每不同分钟的ip以及对应的时间
     ip_list = []
     time_list = []
 
     i = 0
+    # loop through 单个不同分钟的所有访问记录
     while i < len(database.time_index)-1:
+        # 记录两分钟内所有访问的ip
         ip_sublist = []
 
         time_list.append(database.operations[database.time_index[i]][0])
         for j in range(database.time_index[i], database.time_index[i+1]):
             ip_sublist.append(database.operations[j][2])
 
-        # check next minute within 2
+        # 检查下一个不同分钟是否与这一分钟是连续的两分钟，如果是，则添加下一分钟访问的所有所有ip到当前的ip_sublist
         if (database.operations[database.time_index[i+1]][0] - database.operations[database.time_index[i]][0]) <= timedelta(minutes=2):
+            # 防止 index out of bound，单独处理是连续两分钟且第二分钟是最后一个不同分钟的情况
             if i == len(database.time_index)-2:
                 for k in range(database.time_index[i+1], len(database.operations)):
                     ip_sublist.append(database.operations[k][2])
+            # 其余普通情况
             else:
                 for p in range(database.time_index[i+1], database.time_index[i+2]):
                     ip_sublist.append(database.operations[p][2])
         i += 1
-
+        # 将该一分钟/连续两分钟访问的ip sublist添加到总ip list
         ip_list.append(ip_sublist)
 
+    # 防止 index out of bound，单独处理最后一个不同分钟不在连续的两分钟内的情况
     if i < len(database.time_index):
         ip_sublist = []
         for j in range(database.time_index[i], len(database.operations)):
@@ -151,13 +163,14 @@ def account_repeat(database, output):
         ip_list.append(ip_sublist)
         time_list.append(database.operations[database.time_index[i]][0])
 
+    # loop through 每两分钟的ip记录
     for n in range(len(ip_list)):
-
+        # 记录不同的ip
         ip_record = []
         for ip in ip_list[n]:
             if ip not in ip_record:
                 ip_record.append(ip)
-
+        # 如果该两分钟内ip个数达到3个以及上，则输出异常提示
         if len(ip_record) >= 3:
             output.append([database.account, time_list[n], "账号复用"])
             
