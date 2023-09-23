33c03fce2ceed827971d05e6501564f78b64bf9d
Angela <113480613+Angelawork@users.noreply.github.com>
2023-09-23 17:07:25 -0400

    Update database, output and preprocess files with comments (#19)
    * Update all ahead commits (#7)
    
    * Database class draft 1
    
    * Change name & copy paste in tools
    
    * Preprocess.py completed draft 1
    
    * Output.py add close func for writing into file
    
    * Time index error
    
    * Update .gitignore
    
    * Delete __pycache__ directory
    
    * Update unusual_check.py
    
    * Update output.py with comments
    
    * Update preprocess.py with comments
    
    * Update database.py with comments
    
    ---------
    
    Co-authored-by: will-huiyuan <118071171+will-huiyuan@users.noreply.github.com>

--- output.py
+++ output.py
@@ -1,15 +1,18 @@
 class Output:
     def __init__(self, path):
+        # 初始化方法， 定义输出文件的路径与输出的字符串
         self.path = path
-        self.output=""
-    
+        self.output = ""
+
     def append(self, content: list):
-        self.output+=f"{content[0]},{content[1].strftime('%Y-%m-%d %H:%M')},{content[2]}\n"
+        # 向输出内容添加一行记录
+        self.output += f"{content[0]},{content[1].strftime('%Y-%m-%d %H:%M')},{content[2]}\n"
 
     def save(self):
+        # 保存输出到文件
         self.file = open(self.path, 'w', encoding='utf-8')
-        self.file.write("异常账号,异常时间,异常类型\n")
-        self.file.write(f"{self.output}\n")
-        print(f"output file saved at {self.path}")
+        self.file.write("异常账号,异常时间,异常类型\n")  # 写入标题行
+        self.file.write(f"{self.output}\n")  # 写入输出内容
+        print(f"输出文件已保存至 {self.path}")
         self.file.close()
-
+        
